083253e1bac23de13374a8cd61473a46a9c3819e
Mehmet Keskin <keskinm90@gmail.com>
2022-11-01 19:35:21 +0300

    app page created and styled

--- app/app.js
+++ app/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const loginBtn = document.querySelector(".login-btn");
 const app = document.getElementById("app");
 const loginScreen = document.getElementById("login");
@@ -8,6 +10,41 @@ function displayApp(e) {
   e.preventDefault();
   loginScreen.style.display = "none";
   setTimeout(() => {
-    app.style.display = "grid";
+    app.style.display = "flex";
   }, 500);
 }
+
+// APP
+///////////////////////////////
+///////////////////////////////
+// Data
+
+const account1 = {
+  owner: "Jonas Schmedtmann",
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: "Jessica Davis",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: "Steven Thomas Williams",
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: "Sarah Smith",
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
